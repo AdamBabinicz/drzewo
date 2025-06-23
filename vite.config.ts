a815dbd1ebc741524a60b5719e3e322934621a4e
babinicz <babinicz@onet.pl>
2025-06-23 15:48:41 +0200

    sitemap dynamiczna

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import sitemap from "vite-plugin-sitemap";
+
+const dynamicRoutes = [
+  "/drzewo",
+  "/indeks-osob",
+  "/galeria",
+  "/zrodla",
+  "/rod/gierczak",
+  "/rod/ofiara",
+  "/regulamin",
+  "/polityka-prywatnosci",
+  "/tree",
+  "/person-index",
+  "/gallery",
+  "/sources",
+  "/family/gierczak",
+  "/family/ofiara",
+  "/terms-of-service",
+  "/privacy-policy",
+];
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://rodowekorzenie.netlify.app",
+      dynamicRoutes,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(process.cwd(), "client", "src"),
